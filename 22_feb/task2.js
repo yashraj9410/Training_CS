21b9fc509e12170558752afcba3a438fe9b4938e
Yash <yashraj7011@gmail.com>
2023-02-22 15:30:41 +0530

    access modifiers

--- 22_feb/task2.js
+++ 22_feb/task2.js
@@ -2,6 +2,21 @@
 // access modifiers can only bne used in typecript files 
 //access modifiers are denoted with public,private and protected keywords 
 // by default all the members inside of the class are defined publicly 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //public -- can be accessed anywhere in the program 
 // private -- are only accessible within its class scope 
 //protected -- accessible within its class scope as well as the class extending the parent class containing the protected member 
@@ -14,12 +29,30 @@ var Employee = /** @class */ (function () {
     Employee.prototype.greet = function () {
         console.log("Hello, Good Morning ".concat(this.name));
     };
+    Employee.prototype.printid = function () { console.log(this.convertString(this.id)); }; // using the private function inside the class itself
     Employee.prototype.convertString = function (num) {
         return num.toString();
     };
+    Employee.prototype.getid = function (empid) {
+        return this.id = empid;
+    };
     return Employee;
 }());
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Manager.prototype.managerid = function () {
+        console.log("manager id is " + this.getid(1234)); // calling the getid function inside the inherited class manager 
+    };
+    return Manager;
+}(Employee));
 var obj = new Employee("Yash");
 obj.greet();
-var num = 23;
-console.log(convertString(num));
+var obj1 = new Manager("shourya");
+obj1.greet();
+obj1.managerid();
+//console.log(obj.convertString());
+// let num = 23;
+// console.log(convertString(num));                                    // this will return an error message because converttoString is an private method and cannot be used outside the class
--- 22_feb/task2.ts
+++ 22_feb/task2.ts
@@ -12,30 +12,43 @@
 
 class Employee{
 
-    public name:string;                    // available publicly
-    
+    public name:string;        
+    public id:Int16Array;            // available publicly
     constructor(name:string){             //default constructor parameter type is string 
         this.name = name;
     }
-
     //making a public function to greet the emplyee with name 
     greet() {                                                        //default access of greet function is also public 
         console.log(`Hello, Good Morning ${this.name}`)        
     }
+    printid(){console.log(this.convertString(this.id));}             // using the private function inside the class itself
 
+    //private
     private convertString(num){                                      // can only be used within its class scope/boundary
         return num.toString();
     }
 
-    protected convertString(num){                                      // can only be used within its class scope/boundary
-        return num.toString();
+    //protected
+    protected getid(empid){                                               // protected method can only be used within its class or in the inherited class
+        return this.id = empid;
     }
-
-
 }
+
+// creating a inherited class Manager 
+class Manager extends Employee{
+    managerid(){
+        console.log("manager id is "+ this.getid(1234));                // calling the getid function inside the inherited class manager 
+    }
+}
+
 
 let obj = new Employee("Yash");
 obj.greet();
 
+let obj1 = new Manager("shourya");
+obj1.greet();
+obj1.managerid();
+
+//console.log(obj.convertString());
 // let num = 23;
 // console.log(convertString(num));                                    // this will return an error message because converttoString is an private method and cannot be used outside the class
