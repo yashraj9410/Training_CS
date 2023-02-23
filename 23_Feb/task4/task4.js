e14c7a08ce796cae787f800850cbee98010da86f
Yash <yashraj7011@gmail.com>
2023-02-23 13:08:40 +0530

    task2 completed

---
+++ 23_Feb/task4/task4.js
@@ -0,0 +1,26 @@
+// creating a array of places
+let places =["Delhi","Mumbai","Bengaluru","Chennai","Kolkata"];
+
+//creating new  option elements 
+let selectsource = document.getElementById("source");
+let selectdest = document.getElementById("destination");
+
+//looping through the array for source 
+for(let i=0;i<places.length;i++){
+    let opt = document.createElement("option");
+    opt.textContent=places[i];
+    opt.value=places[i];
+    selectsource.appendChild(opt);
+}
+
+//selecting through array for destinations
+for(let i=0;i<places.length;i++){
+    let opt2 = document.createElement("option");
+    opt2.textContent=places[i];
+    opt2.value=places[i];
+    selectdest.appendChild(opt2);
+}
+
+function disable(){
+   
+}
