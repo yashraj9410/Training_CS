f531e7a9522a040edece6a1e5ea81f46030d1204
Yash <yashraj7011@gmail.com>
2023-02-28 18:23:31 +0530

    task1 completed

--- 28_Feb/task1/task1.ts
+++ 28_Feb/task1/task1.ts
@@ -15,13 +15,17 @@ class Person {
     }
 }
 
+// function to display the data entered on th html page 
 function showdata() {
     let fname = (document.getElementById("firstname") as HTMLInputElement).value;
     let lname = (document.getElementById("lastname") as HTMLInputElement).value;
     let age = (document.getElementById("age") as HTMLInputElement).value;
+
+    //creating a new object of class Person
     const newPerson = new Person(fname, lname, parseInt(age))
+
+    //displaying data on the html page 
     let display = (document.getElementById("displaycontent") as HTMLHeadingElement);
     display.innerHTML += newPerson.getPerson + "<br>";
-    console.log(newPerson.getPerson);
 }
 
