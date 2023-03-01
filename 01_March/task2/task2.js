3a74a5dfacf539a237081daa0283c573313844f7
Yash <yashraj7011@gmail.com>
2023-03-01 16:30:23 +0530

    all task completed

---
+++ 01_March/task2/task2.js
@@ -0,0 +1,68 @@
+//Polymorphism , abstraction , Encapsulation in typescript 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// polymorphism , abstraction , encapsulation
+//abstract class for defining methods 
+var Product = /** @class */ (function () {
+    function Product(name, id, exp) {
+        this.name = name;
+        this.id = id;
+        this.exp = exp;
+    }
+    return Product;
+}());
+//class for a guest user 
+var Shampoo = /** @class */ (function (_super) {
+    __extends(Shampoo, _super);
+    function Shampoo(name, id, exp, price) {
+        var _this = _super.call(this, name, id, exp) || this;
+        _this.price = price;
+        return _this;
+    }
+    Shampoo.prototype.display = function () {
+        console.log("Product Name: ".concat(this.name, ", Status: ").concat(this.status(), ",  Price:").concat(this.price));
+    };
+    Shampoo.prototype.available = function () {
+        if (this.name === "Dove") {
+            return true;
+        }
+        return false;
+    };
+    Shampoo.prototype.status = function () {
+        if (this.available()) {
+            return "Available ";
+        }
+        return " Not Available";
+    };
+    return Shampoo;
+}(Product));
+//class for already a user
+var MemberShampoo = /** @class */ (function (_super) {
+    __extends(MemberShampoo, _super);
+    function MemberShampoo() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.discount = 50; // member user will get a discount of 50
+        return _this;
+    }
+    MemberShampoo.prototype.display = function () {
+        console.log("Product Name: ".concat(this.name, ", Status: ").concat(this.status(), ", Discounted Price:").concat(this.price - this.discount));
+    };
+    return MemberShampoo;
+}(Shampoo));
+var guest = new Shampoo("Dove", 1213, "12/10/2025", 249); //creating object of type shampoo for a guest user 
+console.log(guest.display());
+var user = new MemberShampoo("Dove", "1213", "2025", 249); // same product for an old user 
+console.log(user.display());
--- 01_March/task2/task2.ts
+++ 01_March/task2/task2.ts
@@ -1,1 +1,67 @@
 //Polymorphism , abstraction , Encapsulation in typescript 
+
+// polymorphism , abstraction , encapsulation
+
+//abstract class for defining methods 
+abstract class Product {                                                         //creating abstract class product 
+    name: string;
+    protected id: number | string;                                               // using protected members to hide data (Encapsulation )
+    exp: string;
+    constructor(name: string, id: number | string, exp: string) {
+        this.name = name;
+        this.id = id;
+        this.exp = exp;
+    }
+    protected abstract available(): boolean;                                     // abstract method to check availability of product
+
+    protected abstract status(): string;
+
+    public abstract display(): void;                                                // returning the status to customer 
+}
+
+
+//class for a guest user 
+class Shampoo extends Product {
+    protected price: number;
+    constructor(name: string, id: number | string, exp: string, price: number) {
+        super(name, id, exp);
+        this.price = price;
+    }
+
+    public display(): void {
+        console.log(`Product Name: ${this.name}, Status: ${this.status()},  Price:${this.price}`)
+    }
+
+    available() {                                                       // using the abstract methods 
+        if (this.name === "Dove") { return true }
+        return false;
+
+    }
+
+    status(): string {
+        if (this.available()) { return `Available ` }
+        return ` Not Available`;
+    }
+
+}
+
+//class for already a user
+class MemberShampoo extends Shampoo {
+
+    discount: number = 50;                                                                   // member user will get a discount of 50
+
+    public display(): void {                                                                  // overriding the method (Runtime Polymorphism)                 
+        console.log(`Product Name: ${this.name}, Status: ${this.status()}, Discounted Price:${this.price - this.discount}`)
+    }
+
+}
+
+
+const guest = new Shampoo("Dove", 1213, "12/10/2025", 249)                           //creating object of type shampoo for a guest user 
+console.log(guest.display());
+
+const user = new MemberShampoo("Dove", "1213", "2025", 249);                           // same product for an old user 
+console.log(user.display());
+
+
+export{}
