5c0dc54387d021bd1970f66daae3b273750fe6a5
Yash <yashraj7011@gmail.com>
2023-03-02 13:08:07 +0530

    node modules

--- 02_March/task2/src/task2.ts
+++ 02_March/task2/src/task2.ts
@@ -1,1 +1,8 @@
-console.log("This is analytical charting ")
+// Implementing analytical charting using typescript 
+
+// we can implement the charts from the amcharts module of npm by importing the module in the program 
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+
+
+
