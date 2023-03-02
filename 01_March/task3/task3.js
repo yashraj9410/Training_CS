a8cc7269c94d4ee90fcdd4cc6ad226f9bbc172c1
Yash <yashraj7011@gmail.com>
2023-03-02 10:31:49 +0530

    completed

--- 01_March/task3/task3.js
+++ 01_March/task3/task3.js
@@ -1,11 +1,13 @@
+"use strict";
+exports.__esModule = true;
 //creating a string array
 var arr = ['1', 'a', '2', 'b', '3', 'c', '4', 'd', '5', 'e']; //given array
-var newarr = []; //creating new array to push elements 
+var newArr = []; //creating new array to push elements 
 for (var i = 0; i < arr.length; i++) {
     if (isNaN(parseInt(arr[i]))) {
-        newarr.push(arr[i].toUpperCase());
+        newArr.push(arr[i].toUpperCase());
         continue;
     } // checking for string and integers value 
-    newarr.push(parseInt(arr[i])); // pushing the int after parsing it   
+    newArr.push(parseInt(arr[i])); // pushing the int after parsing it   
 }
-console.log(newarr); // printing the new array 
+console.log(newArr); // printing the new array 
