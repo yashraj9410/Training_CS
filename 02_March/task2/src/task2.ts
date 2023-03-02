fa84b2a2db9d2919d105ec0a0edb6aa6c47dd829
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-03-02 14:27:51 +0530

    Delete 02_March/task2 directory

--- 02_March/task2/src/task2.ts
+++
@@ -1,8 +0,0 @@
-// Implementing analytical charting using typescript 
-
-// we can implement the charts from the amcharts module of npm by importing the module in the program 
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-
-
-
