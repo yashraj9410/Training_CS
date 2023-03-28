c9221c3ebb07f8a18ee0a2c4efc227fefb717820
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-03-28 12:41:47 +0530

    status code

--- March_Tasks/27_march/src/controller/user-controller.ts
+++ March_Tasks/27_march/src/controller/user-controller.ts
@@ -1,8 +1,7 @@
 // creating the controller methods for the model i.e for querying 
 import User from '../model/user-model'
 import express, {Request , Response} from 'express';
-
-
+import { body,validationResult } from 'express-validator';
 // creating a new user i the database 
 export const createUser = (req:Request,res:Response) => {
     const data = req.body ;
