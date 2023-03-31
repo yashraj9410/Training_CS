fa9a8717972c5525e07d9980ef72796d0d0d35b0
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-03-31 16:01:43 +0530

    updated hasone relation in controller

--- March_Tasks/29_March/src/controller/profile-controller.ts
+++ March_Tasks/29_March/src/controller/profile-controller.ts
@@ -11,19 +11,26 @@ export const createProfile = async(req:Request,res:Response) => {
     const {id} = req.params
     const userid = req.params.id;
     const data = req.body;
-    console.log(id);
-    data.userId = userid;    // the data.userid is the foreign key referencing to the user table 
+    
+     // checking if a profile already exists by the given user id (because we have a one to one relation )
+     Profile.findOne({where:{userId:userid}})
+     .then(user => {
+         if(user){
+            return res.status(403).send("Profile Already exists for the user");
+         }else{
+            data.userId = userid;    // the data.userid is the foreign key referencing to the user table 
 
-    // check if a user exists y thr id
-    User.findByPk(id)
-    .then(result => {
-        if(result){
-            Profile.create(data)
-            .then(data => res.status(201).send({data}))
-            .catch(err => res.status(400).send("Profile data not added to the database"))
-        }
-    })
-    .catch(err => res.status(404).send("No User found with this id"))
+            User.findByPk(id)        // check if user exists by the id 
+            .then(result => {
+                if(result){
+                    Profile.create(data)
+                    .then(data => res.status(201).send({data}))
+                    .catch(err => res.status(400).send("Profile data not added to the database"))
+                }
+            })
+            .catch(err => res.status(404).send("No User found with this id"))
+         }
+     })
 }
 
 
@@ -35,10 +42,10 @@ export const readProfile = async(req:Request,res:Response) => {
     User.findOne({where:{id}})
     .then(data => {
         if(data){
-            Profile.findAll({where:{id}})
-            .then(profiles => {
-                if(profiles){
-                    res.status(200).send({profiles});
+            Profile.findAll({where:{}})
+            .then(profile => {
+                if(profile){
+                    res.status(200).send({profile});
                 }else {
                     res.status(404).send("No Profile exist for the user");
                 }
@@ -56,12 +63,13 @@ export const updateProfile = async(req:Request,res:Response) => {
 // deleting the profile corresponding to the user 
 export const deleteProfile = async(req:Request,res:Response) => {
     const {id} = req.params;
+    const userid = req.params.id;
 
     ///checking for user with that id 
     User.findOne({where:{id}})
     .then(data => {
         if(data){
-            Profile.destroy({where:{id}})
+            Profile.destroy({where:{userId:userid}})
             .then(profiles => {
                 if(profiles){
                     res.status(200).send("Profile deleted successfully ");
