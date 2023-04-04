23230c4f96af466d5bac52f4defe85609ed18d66
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-04 13:58:19 +0530

    implementing transactions

--- March_Tasks/29_March/src/controller/user-transaction-controller.ts
+++ March_Tasks/29_March/src/controller/user-transaction-controller.ts
@@ -3,3 +3,22 @@
 import User from '../model/user-model';
 
 import {Request, Response } from 'express';
+
+import db from '../middleware/connection';
+
+
+// using the db.transaction function 
+
+export const create = async(req:Request,res:Response) => {
+    const data = req.body;
+    try{
+        await db.transaction(async(t) => {                           // here we have defined a transaction 
+            await User.create(data,{transaction:t})                  // these are the operations under the transaction , autorollback
+                .then(data => res.status(201).send(data))
+                .catch(err => res.status(400).send(err));
+        })
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+}
