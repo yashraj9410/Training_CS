c3083bc8546f5e69182dae3352026d123cdf9b79
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-05 13:57:39 +0530

    swagger setup

---
+++ April_Tasks/04_April/src/utils/swaggerDef.ts
@@ -0,0 +1,19 @@
+import { Options } from 'swagger-jsdoc';
+
+//defining the swagger option
+export const swaggerOptions: Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      version: '1.0.0',
+      description: 'A sample API to demonstrate Swagger with Express and TypeScript'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3300'
+      }
+    ]
+  },
+  apis: ['../routes/*.ts']
+};
