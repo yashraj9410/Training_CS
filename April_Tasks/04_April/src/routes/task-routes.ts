f1779c95e3613736df549518498d7123f8f7e94d
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-05 12:54:58 +0530

    modified create Task controller

--- April_Tasks/04_April/src/routes/task-routes.ts
+++ April_Tasks/04_April/src/routes/task-routes.ts
@@ -2,7 +2,7 @@
 import express from 'express'
 import { TaskSchema,validateSchema } from '../middleware/schema-validator'
 import {verifyToken} from '../middleware/validateToken'
-import { createTask, readTask } from '../controller/task-controller';
+import { createTask, deleteTask, readTask } from '../controller/task-controller';
 //import { read } from 'fs';
 
 const router = express.Router();
@@ -10,5 +10,5 @@ const router = express.Router();
 router.use(verifyToken);
 router.post("/", TaskSchema.checkTask ,validateSchema, createTask);      // registering user using the transaction controller 
 router.get("/", readTask);
-
+router.delete("/:id", deleteTask)
 export default router ;
