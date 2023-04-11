16f00eabca199d3399531fe41f3fc3aef8bac7ff
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-11 12:15:19 +0530

    added task status model

---
+++ April_Tasks/04_April/src/model/taskStatus-model.ts
@@ -0,0 +1,26 @@
+// creating a mode for the task status assigned to the user 
+
+import {Table , Column , BelongsTo , HasOne, DataType, ForeignKey , Model, HasMany, AllowNull} from 'sequelize-typescript'
+import Task from './task-model'
+
+@Table({
+    timestamps:false
+})
+class Task_Status extends Model {
+    @Column({
+        type:DataType.TEXT,
+        allowNull:false
+    })
+    declare description:string
+    @Column({
+        type:DataType.BOOLEAN,
+        allowNull:false      
+    })
+    declare isCompleted:boolean
+
+    @ForeignKey(() => Task)
+    @Column({})
+    declare taskId:Number
+    @BelongsTo(() => Task)
+    declare task:Task
+}
