d634132fbc9016de048a7e9919683b0b604669cc
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-11 18:01:22 +0530

    added task status routes and controller

--- April_Tasks/04_April/src/controller/taskStatus-controller.ts
+++ April_Tasks/04_April/src/controller/taskStatus-controller.ts
@@ -12,23 +12,21 @@ export const set_Task_Status = (req:Request,res:Response) => {
     Task.findByPk(taskid)
     .then(task => {
         if(task){
-            Task_Status.create({
-                description:task.description,
-                isCompleted:status,
-                taskId:taskid,
-                assignedBy:task.adminId,
-                assignedTo:task.userId
+            Task_Status.findByPk(taskid)
+            .then(present => {
+                if(present){
+                    res.status(400).send("taskstatus is already present")
+                }else{
+                    Task_Status.create({
+                        description:task.description,
+                        isCompleted:status,
+                        taskId:taskid
+                    })
+                    .then(data => res.status(201).send("task_status created") )
+                    .catch(err => res.status(400).send("Task status not created , bad request"))
+                }
             })
-            .then(data => res.status(201).send("task_status created") )
-            .catch(err => res.status(400).send("Task status not created , bad request"))
         }
     })
     .catch(err => res.status(404).send("No task exist with the given id "))
 } 
-
-// chnag task status so that user/admin can change the status of task 
-
-export const change_task_status = (req:Request , res:Response) => {
-    const id = req.user?.id;
-
-}
