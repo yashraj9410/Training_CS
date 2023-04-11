663c82253d8cea83350ba15f3535aa79602de843
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-11 17:53:09 +0530

    added task status routes and controller

--- April_Tasks/04_April/src/controller/taskStatus-controller.ts
+++ April_Tasks/04_April/src/controller/taskStatus-controller.ts
@@ -15,7 +15,9 @@ export const set_Task_Status = (req:Request,res:Response) => {
             Task_Status.create({
                 description:task.description,
                 isCompleted:status,
-                taskId:taskid
+                taskId:taskid,
+                assignedBy:task.adminId,
+                assignedTo:task.userId
             })
             .then(data => res.status(201).send("task_status created") )
             .catch(err => res.status(400).send("Task status not created , bad request"))
@@ -23,3 +25,10 @@ export const set_Task_Status = (req:Request,res:Response) => {
     })
     .catch(err => res.status(404).send("No task exist with the given id "))
 } 
+
+// chnag task status so that user/admin can change the status of task 
+
+export const change_task_status = (req:Request , res:Response) => {
+    const id = req.user?.id;
+
+}
