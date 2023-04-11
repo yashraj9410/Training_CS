f5ee8ab5b725d3652f7cd4715b9462e467e08b54
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-11 13:02:35 +0530

    added task status routes and controller

--- April_Tasks/04_April/src/controller/taskStatus-controller.ts
+++ April_Tasks/04_April/src/controller/taskStatus-controller.ts
@@ -1,9 +1,25 @@
 //  making controller for the taskStatus model 
+import Task_Status from '../model/taskStatus-model'
 import Task from '../model/task-model'
 import {Request, Response} from 'express'
 import db from '../middleware/connection'
 
 
 export const set_Task_Status = (req:Request,res:Response) => {
-    const status = req.body;   // fetching the status from body
-}
+    const status = req.body.isCompleted;   // fetching the status from body
+    const taskid = req.body.id;            // getting the task id 
+
+    Task.findByPk(taskid)
+    .then(task => {
+        if(task){
+            Task_Status.create({
+                description:task.description,
+                isCompleted:status,
+                taskId:taskid
+            })
+            .then(data => res.status(201).send("task_status created") )
+            .catch(err => res.status(400).send("Task status not created , bad request"))
+        }
+    })
+    .catch(err => res.status(404).send("No task exist with the given id "))
+} 
