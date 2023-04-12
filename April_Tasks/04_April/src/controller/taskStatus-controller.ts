a840cfca40597ff585a567ade14fae768b566438
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-12 10:54:16 +0530

    created change task status function]

--- April_Tasks/04_April/src/controller/taskStatus-controller.ts
+++ April_Tasks/04_April/src/controller/taskStatus-controller.ts
@@ -30,3 +30,19 @@ export const set_Task_Status = (req:Request,res:Response) => {
     })
     .catch(err => res.status(404).send("No task exist with the given id "))
 } 
+
+
+// change task status functionf for the user/admin 
+export const change_task_status = (req:Request, res:Response) => {
+    const taskid = req.body.taskid;
+
+    Task_Status.findByPk(taskid)
+    .then(task => {
+        if(task){
+            Task_Status.update({isCompleted:req.body.status}, {where:{taskId:taskid}})
+            .then(data => res.status(200).send("Task status Updated"))
+            .catch(err => res.status(401).send("Task not updated successfully"))
+        }
+    })
+    .catch(err => res.status(404).send("No task status found please create a task status before updating"))
+}
