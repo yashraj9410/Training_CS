bb26b61674df9cda54766eba5d8f82014ba78c42
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-14 13:49:52 +0530

    updated task status controller and task status routes

--- April_Tasks/04_April/src/routes/taskStatus-route.ts
+++ April_Tasks/04_April/src/routes/taskStatus-route.ts
@@ -2,7 +2,6 @@
 
 import { set_Task_Status } from "../controller/taskStatus-controller";
 import express from 'express'
-import { verifyToken } from "../middleware/validateToken";
 
 const router = express.Router();
 
