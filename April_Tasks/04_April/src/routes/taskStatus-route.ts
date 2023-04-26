c4493ed036cb8665fca2c328c8a6815be88704df
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-26 18:13:38 +0530

    added comment

--- April_Tasks/04_April/src/routes/taskStatus-route.ts
+++ April_Tasks/04_April/src/routes/taskStatus-route.ts
@@ -5,6 +5,6 @@ import express from 'express'
 
 const router = express.Router();
 
-router.post("/", set_Task_Status); // create status
+router.post("/", set_Task_Status); // create task_status table
 
 export default router;
