70037a82f240c35b1d0b38f61af5f0ecb39b0a23
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-28 14:08:14 +0530

    integrated firebaase push notification

--- April_Tasks/FirebaseExpress/src/firebase/noification.ts
+++ April_Tasks/FirebaseExpress/src/firebase/noification.ts
@@ -1,3 +1,5 @@
+
+import { Request , Response , NextFunction } from 'express'
 import * as admin from 'firebase-admin';
 const serviceAccount = require("./private/serviceAccountKey.json");
 
@@ -5,3 +7,22 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   databaseURL: 'https://push-notification-for-native.firebaseio.com'
 });
+
+
+// Create an instance of the Firebase Messaging service
+const messaging = admin.messaging();
+
+
+export const sendNotification = async (req:Request , res:Response ) => {
+    const appToken = req.body.token;
+
+    const message = {
+      notification: {
+        title: 'New Notification',
+        body: 'This is a test notification'
+      },
+      token: appToken
+    };
+
+    
+}
