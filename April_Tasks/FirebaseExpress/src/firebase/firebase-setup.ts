4cb84f40b74589c70c950dbac9275ef4911544ec
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-04-28 13:57:13 +0530

    integrated firebaase push notification

--- April_Tasks/FirebaseExpress/src/firebase/firebase-setup.ts
+++ April_Tasks/FirebaseExpress/src/firebase/firebase-setup.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '/path/to/serviceAccountKey.json';
+const serviceAccount = require("./private/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: 'https://your-project-id.firebaseio.com'
+  databaseURL: 'https://push-notification-for-native.firebaseio.com'
 });
