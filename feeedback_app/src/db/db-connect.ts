faad2085076ed6d9d66cc45a527ea5dc92341e76
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-04 13:09:47 +0530

    updated the feedback management structure

---
+++ feeedback_app/src/db/db-connect.ts
@@ -0,0 +1,11 @@
+// connecting to the mongodb atlas 
+import mongoose from 'mongoose';
+
+const connect_db = () => {
+
+    mongoose.connect("mongodb+srv://yashraj7011:12345@feedbackmanagement.llintv8.mongodb.net/test")
+    .then(res => console.log("databse connected success fully"))
+    .catch(err => console.log(`Databse not connected : ${err}`))
+}
+
+export default connect_db;
--- feeedback_app/src/server.ts
+++ feeedback_app/src/server.ts
@@ -1,1 +1,14 @@
-// mongodb+srv://yashraj7011:<password>@feedbackmanagement.llintv8.mongodb.net/test
+// creating the server 
+import express from 'express';
+const server =express();
+
+// importing the connect db 
+import connect_db from './db/db-connect';
+
+
+
+// creating the start server function
+connect_db();
+server.listen(4000, () => {
+    console.log("Server running at 4000");
+})
