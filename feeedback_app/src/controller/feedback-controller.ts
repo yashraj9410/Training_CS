58c9b284e3da06823b1c8592bb391d581d4f1e6b
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-05 15:58:55 +0530

    updated feedback controller

--- feeedback_app/src/controller/feedback-controller.ts
+++ feeedback_app/src/controller/feedback-controller.ts
@@ -5,6 +5,15 @@ import FeedbackModel from '../model/feedback_model'
 // create a feedback
 export const getFeedback = async( req:Request, res:Response ) => {
     
+    try {
+        
+        await FeedbackModel.find()
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(404).send(err))
+
+    } catch (error) {
+        res.status(500).send("internal Server error")
+    }
 }
 
 // create a feedback
