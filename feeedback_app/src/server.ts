2120f4e54fde4c8f904a2f8b134c3098437ad31f
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-09 12:41:55 +0530

    updated the server file

--- feeedback_app/src/server.ts
+++ feeedback_app/src/server.ts
@@ -2,18 +2,39 @@
 import express from 'express';
 const server =express();
 
+// importing routes
 import feedback_template_router from './routes/feedback_template_route'
 import feedback_router from './routes/feedback_route'
 
-// importing the connect db 
+// importing the  db config 
 import connect_db from './db/db-connect';
 
-server.use(express.json());
-server.use("/api/feedbackTemplate" , feedback_template_router);
-server.use("/api/feedback" , feedback_router);
 
-// creating the start server function
-connect_db();
-server.listen(4000, () => {
-    console.log("Server running at 4000");
-})
+
+// connect db function 
+const start_server = async() => {
+
+    try {
+
+        await connect_db();
+        server_config();
+
+    } catch (error) {
+        console.log("Error in connecting db")    
+    }
+}
+
+const server_config = () => {
+
+    server.use(express.json());
+
+    server.use("/api/feedbackTemplate" , feedback_template_router);
+    server.use("/api/feedback" , feedback_router);
+
+    server.listen(4000, () => {
+        console.log("Server running at 4000");
+    })
+}
+
+// start the server 
+start_server();
